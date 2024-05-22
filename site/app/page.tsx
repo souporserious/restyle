987480dc46ad2f6d569da416f82984998aa70a4f
Travis Arnold <ftntravis@gmail.com>
2024-05-22 02:01:30 -0700

    add input/output section

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,11 +1,13 @@
 import {
   CodeBlock,
   CodeInline,
+  Copyright,
   GitProviderLink,
   MDXComponents,
+  RenderedHTML,
 } from 'mdxts/components'
-import { Copyright } from 'mdxts/components/Copyright'
 import { MdxtsLogoLink } from 'mdxts/assets'
+import { css } from 'restyle'
 
 import Examples from './Examples.mdx'
 import { FeaturesGrid } from './FeaturesGrid'
@@ -30,15 +32,46 @@ export function Button({
   });
 
   return (
+    <button className={classNames} onClick={onClick}>
+      {children}
+      {styles}
+    </button>
+  );
+}
+`
+
+export function Button({
+  children,
+  padding = '0.5rem 1rem',
+  borderRadius = '4px',
+  backgroundColor = 'dodgerblue',
+  color = 'white',
+  onClick,
+}: {
+  children: React.ReactNode
+  padding?: string
+  borderRadius?: string
+  backgroundColor?: string
+  color?: string
+  onClick?: () => void
+}) {
+  const [classNames, styles] = css({
+    border: 'none',
+    padding,
+    borderRadius,
+    backgroundColor,
+    color,
+  })
+
+  return (
     <>
       <button className={classNames} onClick={onClick}>
         {children}
       </button>
       {styles}
     </>
-  );
+  )
 }
-`
 
 export default function Page() {
   return (
@@ -67,70 +100,98 @@ export default function Page() {
         </p>
       </header>
 
-      <main className="flex flex-col min-h-screen max-w-screen-xl p-8 mx-auto gap-8">
-        <div className="flex-1 grid grid-cols-1 md:grid-cols-8 items-center gap-16">
-          <div className="md:col-span-4 flex flex-col justify-center space-y-6">
-            <div className="space-y-8">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                <img
-                  src="logo-light.png"
-                  alt="restyle logo"
-                  title="restyle"
-                  className="h-10 sm:h-20"
+      <main>
+        <div className="flex flex-col min-h-screen max-w-screen-xl p-8 mx-auto gap-8">
+          <section className="flex-1 grid grid-cols-1 md:grid-cols-8 items-center gap-16">
+            <div className="md:col-span-4 flex flex-col justify-center space-y-6">
+              <div className="space-y-8">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  <img
+                    src="logo-light.png"
+                    alt="restyle logo"
+                    title="restyle"
+                    className="h-10 sm:h-20"
+                  />
+                </h1>
+                <p className="text-4xl font-bold leading-snug">
+                  A <mark className="bg-amber-200">really great</mark> <br />
+                  <mark className="bg-amber-200">CSS-in-JS</mark> library
+                  <br />
+                  for React
+                </p>
+              </div>
+              <div className="flex gap-4">
+                <CodeInline
+                  allowCopy
+                  value={`npm install restyle`}
+                  language="bash"
+                  paddingX="0.8em"
+                  paddingY="0.5em"
                 />
-              </h1>
-              <p className="text-4xl font-bold leading-snug">
-                A <mark className="bg-amber-200">really good</mark> <br />
-                <mark className="bg-amber-200">CSS-in-JS</mark> library
-                <br />
-                for React
-              </p>
+                <GitProviderLink className="flex items-center gap-2 px-6 py-2 font-bold tracking-wider text-white bg-gray-800 rounded-md">
+                  GitHub
+                </GitProviderLink>
+              </div>
             </div>
-            <div className="flex gap-4">
-              <CodeInline
-                allowCopy
-                value={`npm install restyle`}
-                language="bash"
-                paddingX="0.8em"
-                paddingY="0.5em"
+            <CodeBlock
+              value={exampleCode}
+              language="tsx"
+              highlightedLines="1,11-17,20,22"
+              className={{ container: 'md:col-span-4' }}
+            />
+          </section>
+        </div>
+
+        <section className="px-12 py-24 bg-indigo-50">
+          <div
+            css={{
+              display: 'grid',
+              gridTemplateColumns: 'minmax(0, 1fr) minmax(0, 1fr)',
+              gap: '2rem',
+            }}
+          >
+            <div>
+              <h2 className="text-3xl font-extrabold text-violet-400">Input</h2>
+              <CodeBlock
+                value={`${exampleCode}\nexport default function Example() {\nreturn <Button onClick={() => alert()}>Click me!</Button>\n}`}
+                language="tsx"
               />
-              <GitProviderLink className="flex items-center gap-2 px-6 py-2 font-bold tracking-wider text-white bg-gray-800 rounded-md">
-                GitHub
-              </GitProviderLink>
+            </div>
+            <div>
+              <h2 className="text-3xl font-extrabold text-violet-400">
+                Output
+              </h2>
+              <RenderedHTML>
+                <Button>Click me!</Button>
+              </RenderedHTML>
             </div>
           </div>
-          <CodeBlock
-            value={exampleCode}
-            language="tsx"
-            highlightedLines="1,11-17,21,24"
-            className={{ container: 'md:col-span-4' }}
+        </section>
+
+        <FeaturesGrid />
+
+        <section className="prose max-w-screen-md mx-auto px-2 py-24 text-lg">
+          <Examples
+            components={
+              {
+                h2: (props) => (
+                  <h2
+                    {...props}
+                    className="text-3xl font-extrabold text-violet-400"
+                  />
+                ),
+                h3: (props) => (
+                  <h3
+                    {...props}
+                    className="text-2xl font-semibold text-cyan-600"
+                  />
+                ),
+                pre: (props) => <MDXComponents.pre allowErrors {...props} />,
+              } satisfies MDXComponents as any
+            }
           />
-        </div>
+        </section>
       </main>
-
-      <FeaturesGrid />
-
-      <section className="prose max-w-screen-md mx-auto px-2 py-24 text-lg">
-        <Examples
-          components={
-            {
-              h2: (props) => (
-                <h2
-                  {...props}
-                  className="text-3xl font-extrabold text-violet-400"
-                />
-              ),
-              h3: (props) => (
-                <h3
-                  {...props}
-                  className="text-2xl font-semibold text-cyan-600"
-                />
-              ),
-              pre: (props) => <MDXComponents.pre allowErrors {...props} />,
-            } satisfies MDXComponents as any
-          }
-        />
-      </section>
 
       <footer
         className="text-center p-4 bg-slate-50"
