dba88358bb776de7de24fd5489f42680f67e7ca8
Travis Arnold <ftntravis@gmail.com>
2024-05-23 12:22:57 -0700

    add analytics

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   )
 }
