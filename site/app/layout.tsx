6ed96b0fddd9728d9916ae9c638e061d971fbed5
Travis Arnold <ftntravis@gmail.com>
2024-05-28 21:04:39 -0700

    globals -> layout

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
-import './globals.css'
+
+import './layout.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
