b7507e1006d3530dc951215ff015e5e163067b15
Travis Arnold <ftntravis@gmail.com>
2024-05-28 21:04:49 -0700

    add site metadata

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -2,13 +2,15 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 
+import { getSiteMetadata } from '../utils/get-site-metadata'
+
 import './layout.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'restyle',
-  description: 'The simplest way to add CSS styles to your React components.',
+export function generateMetadata(): Metadata {
+  const siteMetadata = getSiteMetadata()
+  return siteMetadata
 }
 
 export default function RootLayout({
---
+++ site/utils/get-site-metadata.ts
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+
+const url = 'https://restyle.dev/'
+
+export function getSiteMetadata({
+  title = 'Restyle - Zero Config CSS for React',
+  description = `The simplest way to add CSS styles to your React components.`,
+  keywords = 'react, css, css-in-js, styling, components, design, systems',
+  ...rest
+}: { title?: string; description?: string } & Omit<
+  Metadata,
+  'title' | 'description'
+> = {}) {
+  return {
+    metadataBase: new URL(url),
+    title,
+    description,
+    keywords,
+    ...rest,
+    twitter: {
+      card: 'summary_large_image',
+      site: '@souporserious',
+      ...rest.twitter,
+    },
+  } satisfies Metadata
+}
