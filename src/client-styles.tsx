4352af57a9b2148a41a092bffe3173090110378c
Robbie Wadley <robbiethewadley@gmail.com>
2025-01-15 21:52:52 -0700

    depth-based precedence system (#24)

--- src/client-styles.tsx
+++ src/client-styles.tsx
@@ -3,7 +3,8 @@ import { useLayoutEffect } from 'react'
 
 import type { CSSRule } from './types.js'
 
-let hasRenderedInitialStyles = false
+let hasRenderedInitialStylesToDepth = -1
+type NestedRules = [CSSRule[], CSSRule[], CSSRule[], NestedRules[]]
 
 /**
  * Renders style elements in order of low, medium, and high precedence.
@@ -19,60 +20,101 @@ let hasRenderedInitialStyles = false
 export function ClientStyles({
   r: rules,
   n: nonce,
+  d: depth = 0,
 }: {
-  r: [CSSRule[], CSSRule[], CSSRule[]]
+  r: NestedRules
   n?: string
+  d?: number
 }) {
-  const [lowRules, mediumRules, highRules] = rules
+  const [lowRules, mediumRules, highRules, nested] = rules
 
-  /* Only render the initial styles once to establish precedence order */
-  if (hasRenderedInitialStyles === false) {
-    useLayoutEffect(() => {
-      hasRenderedInitialStyles = true
-    }, [])
-  }
+  /* Only render the initial styles for each depth once to establish precedence order */
+  const hasRenderedThisDepth = hasRenderedInitialStylesToDepth >= depth
+  useLayoutEffect(() => {
+    hasRenderedInitialStylesToDepth = Math.max(
+      depth,
+      hasRenderedInitialStylesToDepth
+    )
+  }, [depth])
 
   /* Don't send undefined nonce to reduce serialization size */
   const sharedProps = nonce ? { nonce } : {}
 
+  const depthString = depth === 0 ? '' : depth.toString()
+  const levels = {
+    low: `rsl${depthString}`,
+    med: `rsm${depthString}`,
+    high: `rsh${depthString}`,
+  }
+
   return (
     <>
       {lowRules.length === 0 ? (
-        hasRenderedInitialStyles ? null : (
-          <style href="rsli" precedence="rsl" {...sharedProps} />
+        hasRenderedThisDepth ? null : (
+          <style
+            href={`${levels.low}i`}
+            precedence={levels.low}
+            {...sharedProps}
+          />
         )
       ) : (
         lowRules.map(([className, rule], index) => (
-          <style href={className} precedence="rsl" key={index} {...sharedProps}>
+          <style
+            href={className}
+            precedence={levels.low}
+            key={index}
+            {...sharedProps}
+          >
             {rule}
           </style>
         ))
       )}
 
       {mediumRules.length === 0 ? (
-        hasRenderedInitialStyles ? null : (
-          <style href="rsmi" precedence="rsm" {...sharedProps} />
+        hasRenderedThisDepth ? null : (
+          <style
+            href={`${levels.med}i`}
+            precedence={levels.med}
+            {...sharedProps}
+          />
         )
       ) : (
         mediumRules.map(([className, rule], index) => (
-          <style href={className} precedence="rsm" key={index} {...sharedProps}>
+          <style
+            href={className}
+            precedence={levels.med}
+            key={index}
+            {...sharedProps}
+          >
             {rule}
           </style>
         ))
       )}
 
-      {highRules.length > 0
-        ? highRules.map(([className, rule], index) => (
-            <style
-              href={className}
-              precedence="rsh"
-              key={index}
-              {...sharedProps}
-            >
-              {rule}
-            </style>
-          ))
-        : null}
+      {highRules.length === 0 ? (
+        hasRenderedThisDepth ? null : (
+          <style
+            href={`${levels.high}i`}
+            precedence={levels.high}
+            {...sharedProps}
+          />
+        )
+      ) : (
+        highRules.map(([className, rule], index) => (
+          <style
+            href={className}
+            precedence={levels.high}
+            key={index}
+            {...sharedProps}
+          >
+            {rule}
+          </style>
+        ))
+      )}
+
+      {nested.map((nested, index) => (
+        <ClientStyles key={index} d={depth + 1} n={nonce} r={nested} />
+      ))}
     </>
   )
 }
