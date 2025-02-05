0cef68a3837f6e9036bea78e623d77132c85b7d8
Robbie Wadley <robbiethewadley@gmail.com>
2025-02-04 22:14:45 -0700

    types: improve the types returned by styled (#32)

---
+++ src/styled.test-d.tsx
@@ -0,0 +1,436 @@
+import { expectTypeOf, test } from 'vitest'
+import { styled } from './styled.js'
+import { Component, type FC } from 'react'
+
+test('basic component type is preserved', () => {
+  const component = ({
+    className,
+    name,
+  }: {
+    className: string
+    name: string
+  }) => <div className={className}>{name}</div>
+  const extended = styled(component, {})
+
+  expectTypeOf(extended).toMatchTypeOf<typeof component>()
+})
+
+test('additional property types are added', () => {
+  const component = ({
+    className,
+    name,
+  }: {
+    className: string
+    name: string
+  }) => <div className={className}>{name}</div>
+  const extended = styled(component, ({ color }: { color: string }) => ({
+    color,
+  }))
+
+  expectTypeOf(extended).toMatchTypeOf<
+    FC<{
+      className: string
+      name: string
+      color: string
+    }>
+  >()
+
+  const extended2 = styled('div', ({ color }: { color: string }) => ({
+    color,
+  }))
+
+  expectTypeOf(extended2).toMatchTypeOf<FC<{ color: string }>>()
+})
+
+test('style props are filtered from the component props', () => {
+  const component = ({
+    className,
+    color,
+  }: {
+    className: string
+    color?: number
+  }) => <div className={className}>{color}</div>
+  const extended = styled(component, ({ color }: { color: string }) => ({
+    color,
+  }))
+
+  expectTypeOf(extended).toMatchTypeOf<
+    FC<{
+      className: string
+      color: string
+    }>
+  >()
+
+  const extended2 = styled('div', ({ onClick }: { onClick: string }) => ({
+    color: onClick,
+  }))
+  expectTypeOf(extended2).toMatchTypeOf<FC<{ onClick: string }>>()
+})
+
+test('style props are not allowed to break the component type', () => {
+  const component = ({
+    className,
+    color,
+  }: {
+    className: string
+    color: number
+  }) => <div className={className}>{color}</div>
+
+  // @ts-expect-error extending with color would make color always undefined, which is a type error (color must be a number)
+  const extended = styled(component, ({ color }: { color: string }) => ({
+    color,
+  }))
+})
+
+test('extra properties are not allowed', () => {
+  const Component = ({ className }: { className: string }) => (
+    <div className={className} />
+  )
+  const Extended = styled(Component, { color: 'red' })
+  const ExtendedWithProps = styled(
+    Extended,
+    ({ color }: { color: string }) => ({
+      color,
+    })
+  )
+
+  const basicTest = (
+    <Extended
+      className="abc"
+      // @ts-expect-error name does not exist on type
+      name="abc"
+    />
+  )
+  const propsTest = (
+    <ExtendedWithProps
+      className="abc"
+      // @ts-expect-error name does not exist on type
+      name="abc"
+      color="red"
+    />
+  )
+
+  const Extended2 = styled('div', { color: 'red' })
+  const ExtendedWithProps2 = styled(
+    Extended2,
+    ({ color }: { color: string }) => ({
+      color,
+    })
+  )
+
+  const basicTest2 = (
+    <Extended2
+      className="abc"
+      // @ts-expect-error name does not exist on type
+      name="abc"
+    />
+  )
+  const propsTest2 = (
+    <ExtendedWithProps2
+      className="abc"
+      // @ts-expect-error name does not exist on type
+      name="abc"
+      color="red"
+    />
+  )
+})
+
+test('extra properties are permitted if style props allow them', () => {
+  const Component = ({ className }: { className: string }) => (
+    <div className={className} />
+  )
+  const Extended = styled(Component, { color: 'red' })
+  const ExtendedWithProps = styled(
+    Extended,
+    ({ color }: { [key: string]: string }) => ({
+      color,
+    })
+  )
+
+  const basicTest = (
+    <Extended
+      className="abc"
+      // @ts-expect-error name does not exist on type
+      name="abc"
+    />
+  )
+  const propsTest = <ExtendedWithProps className="abc" name="abc" color="red" />
+
+  const Extended2 = styled('div', { color: 'red' })
+  const ExtendedWithProps2 = styled(
+    Extended2,
+    ({ color }: { [key: string]: string }) => ({
+      color,
+    })
+  )
+
+  const basicTest2 = (
+    <Extended2
+      className="abc"
+      // @ts-expect-error name does not exist on type
+      name="abc"
+    />
+  )
+  const propsTest2 = (
+    <ExtendedWithProps2 className="abc" name="abc" color="red" />
+  )
+})
+
+test('style props are required to be a record', () => {
+  const component = ({ className }: { className: string }) => (
+    <div className={className} />
+  )
+
+  // allowed
+  styled(component, (props: unknown) => ({ color: 'red' }))
+  styled(component, (props: never) => ({ color: 'red' }))
+
+  // disallowed
+  // @ts-expect-error
+  styled(component, (props: number) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled(component, (props: string) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled(component, (props: Map<string, string>) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled(component, (props: []) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled(component, (props: Set<string>) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled(component, (props: undefined) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled(component, (props: null) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled(component, (props: object) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled(component, (props: { color: string } | undefined) => ({
+    color: 'red',
+  }))
+
+  // allowed
+  styled('div', (props: unknown) => ({ color: 'red' }))
+  styled('div', (props: never) => ({ color: 'red' }))
+
+  // disallowed
+  // @ts-expect-error
+  styled('div', (props: number) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled('div', (props: string) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled('div', (props: Map<string, string>) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled('div', (props: []) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled('div', (props: Set<string>) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled('div', (props: undefined) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled('div', (props: null) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled('div', (props: object) => ({ color: 'red' }))
+  // @ts-expect-error
+  styled('div', (props: { color: string } | undefined) => ({
+    color: 'red',
+  }))
+})
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      disallowed: { color: string }
+    }
+  }
+}
+
+test('component is required to have a className prop', () => {
+  const withClassName = (props: { className?: string }) => <div />
+  const withoutClassName = (props: { color: string }) => <div />
+
+  // allowed
+  styled(withClassName)
+  styled('div')
+
+  const test = (
+    <>
+      <disallowed color="red" />
+    </>
+  )
+
+  // not allowed
+  // @ts-expect-error
+  styled(withoutClassName)
+  // @ts-expect-error
+  styled('disallowed')
+})
+
+test('generic types are preserved without style props', () => {
+  const Component = <SomeText extends string>({
+    id,
+    one,
+    two,
+    className,
+  }: {
+    id: `id-${SomeText}`
+    one: NoInfer<SomeText>
+    two: NoInfer<SomeText>
+    className?: string
+  }) => <></>
+  const Extended = styled(Component)
+
+  const test = (
+    <>
+      <Component<'abc'> id="id-abc" one="abc" two="abc" />
+      <Extended<'abc'> id="id-abc" one="abc" two="abc" />
+    </>
+  )
+  Component({
+    id: 'id-abc',
+    one: 'abc',
+    // @ts-expect-error types require 'abc'
+    two: 'xyz',
+  })
+  Extended({
+    id: 'id-abc',
+    one: 'abc',
+    // @ts-expect-error types require 'abc'
+    two: 'xyz',
+  })
+})
+
+test('generic types are preserved with style props', () => {
+  const Component = <SomeText extends string>({
+    id,
+    one,
+    two,
+    className,
+  }: {
+    id: `id-${SomeText}`
+    one: NoInfer<SomeText>
+    two: NoInfer<SomeText>
+    className?: string
+  }) => <></>
+  const Extended = styled(Component, ({ color }: { color: string }) => ({
+    color,
+  }))
+
+  const test = (
+    <>
+      <Component<'abc'> id="id-abc" one="abc" two="abc" />
+      <Extended<'abc'> id="id-abc" one="abc" two="abc" color="red" />
+    </>
+  )
+  Component({
+    id: 'id-abc',
+    one: 'abc',
+    // @ts-expect-error types require 'abc'
+    two: 'xyz',
+  })
+  Extended({
+    color: 'red',
+    id: 'id-abc',
+    one: 'abc',
+    // @ts-expect-error types require 'abc'
+    two: 'xyz',
+  })
+})
+
+test('generic types are preserved even when partially overwritten by style props', () => {
+  const component = <SomeText extends string>({
+    id,
+    one,
+    two,
+    className,
+  }: {
+    id: `id-${SomeText}`
+    one?: NoInfer<SomeText>
+    two: NoInfer<SomeText>
+    className?: string
+  }) => <></>
+  const extended = styled(component, ({ one }: { one: string }) => ({
+    color: one,
+  }))
+
+  component({
+    id: 'id-abc',
+    one: 'abc',
+    // @ts-expect-error types require 'abc'
+    two: 'xyz',
+  })
+  extended({
+    id: 'id-abc',
+    one: 'xyz', // <- allowable because the type comes from style props
+    // @ts-expect-error types require 'abc'
+    two: 'xyz',
+  })
+})
+
+test('generic types with multiple generics are preserved', () => {
+  const Component = <SomeText extends string, AnotherText extends string>({
+    id,
+    one,
+    two,
+    className,
+  }: {
+    id: `id-${SomeText}-${AnotherText}`
+    one: NoInfer<SomeText>
+    two: NoInfer<AnotherText>
+    className?: string
+  }) => <></>
+  const Extended = styled(Component, ({ color }: { color: string }) => ({
+    color,
+  }))
+
+  const test = (
+    <>
+      <Component<'abc', 'xyz'> id="id-abc-xyz" one="abc" two="xyz" />
+      <Extended<'abc', 'xyz'> id="id-abc-xyz" one="abc" two="xyz" color="red" />
+    </>
+  )
+  Component({
+    id: 'id-abc-xyz',
+    one: 'abc',
+    // @ts-expect-error types require 'abc'
+    two: 'abc',
+  })
+  Extended({
+    color: 'red',
+    id: 'id-abc-xyz',
+    one: 'abc',
+    // @ts-expect-error types require 'abc'
+    two: 'abc',
+  })
+})
+
+test('class components work', () => {
+  class WithClass extends Component<{ className?: string }> {
+    override render() {
+      return <div className={this.props.className} />
+    }
+  }
+  const Extended = styled(WithClass, { color: 'red' })
+
+  const test = (
+    <>
+      <Component className="abc" />
+      <Extended className="abc" />
+    </>
+  )
+})
+
+test('css prop is added to the component props', () => {
+  const Test = (props: { className?: string }) => (
+    <div className={props.className} />
+  )
+  const Extended = styled(Test, { color: 'red' })
+
+  const test = (
+    <>
+      <Test
+        className="abc"
+        // @ts-expect-error css prop does not exist yet
+        css={{ color: 'red' }}
+      />
+      <Extended className="abc" css={{ color: 'red' }} />
+    </>
+  )
+})
--- src/styled.tsx
+++ src/styled.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
 
 import { css } from './css.js'
-import type { AcceptsClassName, CSSObject } from './types.js'
+import type {
+  AcceptsClassName,
+  CompatibleProps,
+  CSSObject,
+  RestrictToRecord,
+  SimpleFunctionComponent,
+} from './types.js'
+import type { JSX } from './jsx-runtime.js'
 
 /**
  * Creates a JSX component that forwards a `className` prop with the generated
@@ -10,30 +17,70 @@ import type { AcceptsClassName, CSSObject } from './types.js'
  *
  * Note, the provided component must accept a `className` prop.
  */
-export function styled<
-  ComponentType extends React.ElementType,
-  StyleProps extends Record<string, unknown>,
->(
-  Component: AcceptsClassName<ComponentType>,
-  styles?: CSSObject | ((props: StyleProps) => CSSObject)
+export function styled<Props extends { className?: string }, StyleProps>(
+  Component: SimpleFunctionComponent<Props>,
+  styles?:
+    | CSSObject
+    | ((
+        // style props will be omitted from the props passed to the component
+        // so we need to ensure that we won't break the type the component expects
+        props: CompatibleProps<
+          NoInfer<Props>,
+          // style props cannot extend from Record without unintended consequences
+          // so we restrict them here instead
+          RestrictToRecord<StyleProps>
+        >
+      ) => CSSObject)
+): SimpleFunctionComponent<
+  Omit<Props, keyof StyleProps> & {
+    css?: CSSObject
+    className?: string
+  } & StyleProps
+>
+
+// in order to preserve generic types, the signatures for function and intrinsic/class components must be entirely separate
+// having any sort of union type for `Component` will break generic types
+export function styled<TagName extends keyof JSX.IntrinsicElements, StyleProps>(
+  Component:
+    | AcceptsClassName<TagName>
+    | React.ComponentClass<{ className?: string }>,
+  styles?:
+    | CSSObject
+    | ((
+        // see above
+        props: CompatibleProps<
+          React.ComponentProps<TagName>,
+          // see above
+          RestrictToRecord<StyleProps>
+        >
+      ) => CSSObject)
+): SimpleFunctionComponent<
+  Omit<React.ComponentProps<TagName>, keyof StyleProps> & {
+    css?: CSSObject
+    className?: string
+  } & StyleProps
+>
+
+export function styled(
+  Component: string | SimpleFunctionComponent<unknown>,
+  styles?: CSSObject | ((props: unknown) => CSSObject)
 ) {
   return ({
     className: classNameProp,
     css: cssProp,
     ...props
-  }: React.ComponentProps<ComponentType> &
-    StyleProps & {
-      className?: string
-      css?: CSSObject
-    }) => {
+  }: {
+    className?: string
+    css?: CSSObject
+  }) => {
     let parsedStyles: CSSObject
 
     if (typeof styles === 'function') {
-      const accessedProps = new Set<keyof StyleProps>()
-      const proxyProps = new Proxy(props as unknown as StyleProps, {
+      const accessedProps = new Set<string>()
+      const proxyProps = new Proxy(props, {
         get(target, prop: string) {
           accessedProps.add(prop)
-          return target[prop]
+          return target[prop as keyof typeof target]
         },
       })
 
@@ -41,7 +88,7 @@ export function styled<
 
       // Filter out accessed props from the original props
       accessedProps.forEach((prop) => {
-        delete (props as unknown as StyleProps)[prop]
+        delete props[prop as keyof typeof props]
       })
     } else {
       parsedStyles = styles || {}
